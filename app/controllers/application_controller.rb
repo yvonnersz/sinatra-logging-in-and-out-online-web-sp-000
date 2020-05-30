333e1cbe467da46afab9eb776a9ba626145b0a94
Yvonne M Pham <yvonnersz@gmail.com>
2020-05-30 05:05:28 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,8 +21,7 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    if Helpers.is_logged_in?(session)
-      @user = Helpers.current_user(session)
+    if Helpers.is_logged_in?(session) == true
       erb :account
     else
       erb :error
